48e87cf5b0b13300e0070749508ac7dcb3de9a3f
kartiksingh01 <kskartiksingh22@gmail.com>
2022-10-17 22:22:48 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'weatherapp'`, () => {
+  it(`should have as title 'weather'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('weatherapp');
+    expect(app.title).toEqual('weather');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('weatherapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('weather app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { AppService } from './services/app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weatherapp';
+export class AppComponent implements OnInit{
+  title = 'weather';
+  weatherData?:WeatherData;
+  isDay:boolean=false;
+  dayCondition:string='';
+  cityName:string="Delhi";
+  constructor(private appService:AppService){}
+  ngOnInit(){
+    this.getWeatherData(this.cityName);
+    this.cityName="";
+  }
+  getNewData(){
+      this.getWeatherData(this.cityName);
+      this.cityName="";
+  }
+  private getWeatherData(cityName:string){
+    this.appService.getWeatherData(cityName).subscribe({
+      next:(response)=>{
+        this.weatherData=response;
+        if(response.current.is_day==0){
+          this.isDay=false;
+        } else{
+          this.isDay=true;
+        }
+        this.dayCondition=this.weatherData.current.condition.text.toLowerCase();
+        if(this.dayCondition.includes('mist')){
+          this.dayCondition ="mist";
+        }
+        else if(this.dayCondition.includes('clear')){
+          this.dayCondition ="clear";
+        }
+        else if(this.dayCondition.includes('cloudy')){
+          this.dayCondition ="cloudy";
+        }
+        else if(this.dayCondition.includes('rain')){
+          this.dayCondition ="rain";
+        }
+        else if(this.dayCondition.includes('rain')&&this.isDay){
+          this.dayCondition ="rain-night";
+        }
+        else if(this.dayCondition.includes('snow')){
+          this.dayCondition ="snow";
+        }
+        else if(this.dayCondition.includes('thunderstorm')){
+          this.dayCondition ="thunderstorm";
+        }
+      }
+    })
+  }
 }
