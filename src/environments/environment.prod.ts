971f4c4e48f8cd9416781a9e69db929dce4aab84
kartiksingh01 <kskartiksingh22@gmail.com>
2022-10-17 22:51:41 +0530

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl : "",
+  XRapidAPIHostHeaderName:"",
+
+  XRapidAPIHostValue:"",
+  XRapidAPIKeyHeaderName:"",
+  XRapidAPIKeyValue:"",
 };
